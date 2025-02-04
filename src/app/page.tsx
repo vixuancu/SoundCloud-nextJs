de155e22f52dc0a8f997283ff6221d39edfdc1bf
vixuancu <vixuancu2004@gmail.com>
2025-02-04 20:51:13 +0700

    Design Appbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import AppHeader from "@/components/header/app.header";
+
 export default function HomePage() {
-  return <div>hello world</div>;
+  return (
+    <div>
+      <AppHeader />
+    </div>
+  );
 }
