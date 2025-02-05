2405a5fcdb935c4f9881064e394830a245b98829
vixuancu <vixuancu2004@gmail.com>
2025-02-05 23:26:37 +0700

    react Ref

--- src/components/track/wave.track.tsx
+++ src/components/track/wave.track.tsx
@@ -1,16 +1,19 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import WaveSurfer from "wavesurfer.js";
 const WaveTrack = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
-    // chạy chế độ dev thì useEffect chạy 2 lần
-    const wavesurfer = WaveSurfer.create({
-      container: document.getElementById("hoidanit")!, // ! nghĩa tham số không null
-      waveColor: "rgb(200, 0, 200)",
-      progressColor: "rgb(100, 0, 100)",
-      url: "/audio/CHILL.mp3",
-    });
+    if (containerRef.current) {
+      // chạy chế độ dev thì useEffect chạy 2 lần
+      const wavesurfer = WaveSurfer.create({
+        container: containerRef.current, // ! nghĩa tham số không null. containerRef.current trả về HTML
+        waveColor: "rgb(200, 0, 200)",
+        progressColor: "rgb(100, 0, 100)",
+        url: "/audio/CHILL.mp3",
+      });
+    }
   }, []);
-  return <div id="hoidanit">WaveTrack</div>;
+  return <div ref={containerRef}>WaveTrack</div>;
 };
 export default WaveTrack;
