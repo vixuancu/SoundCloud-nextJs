914f7bc14944988bec2822c492cc1229ca885938
vixuancu <vixuancu2004@gmail.com>
2025-02-07 21:30:18 +0700

    Login with Credentials

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,10 +2,50 @@ import { sendRequest } from "@/utils/api";
 import NextAuth, { AuthOptions } from "next-auth";
 import { JWT } from "next-auth/jwt";
 import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
 export const authOptions: AuthOptions = {
   secret: process.env.NO_SECRET,
   // Configure one or more authentication providers
   providers: [
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. 'Sign in with...')
+      name: "username&password",
+      // The credentials is used to generate a suitable form on the sign in page.
+      // You can specify whatever fields you are expecting to be submitted.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        username: { label: "Tên đăng nhập", type: "text" },
+        password: { label: "Mật Khẩu", type: "password" },
+      },
+      async authorize(credentials, req) {
+        // You need to provide your own logic here that takes the credentials
+        // submitted and returns either a object representing a user or value
+        // that is false/null if the credentials are invalid.
+        // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
+        // You can also use the `req` object to obtain additional parameters
+        // (i.e., the request IP address)
+
+        //
+        const res = await sendRequest<IBackendRes<JWT>>({
+          url: "http://localhost:8000/api/v1/auth/login",
+          method: "POST",
+          body: {
+            username: credentials?.username,
+            password: credentials?.password,
+          },
+        });
+        //
+
+        // If no error and we have user data, return it
+        if (res && res.data) {
+          return res.data as any; // dữ liệu ở đây là user
+        } else {
+          // Return null if user data could not be retrieved
+          return null;
+        }
+      },
+    }),
     GithubProvider({
       clientId: process.env.GITHUB_ID!, // thêm dấu ! để không báo lỗi undefined
       clientSecret: process.env.GITHUB_SECRET!,
@@ -14,16 +54,31 @@ export const authOptions: AuthOptions = {
   ],
   callbacks: {
     async jwt({ token, user, account, profile, isNewUser, trigger }) {
-      if (trigger === "signIn" && account?.provider === "github") {
+      if (trigger === "signIn" && account?.provider !== "credentials") {
+        // dùng debug ở server để biết thuộc tính
         //todo
         const res = await sendRequest<IBackendRes<JWT>>({
           url: "http://localhost:8000/api/v1/auth/social-media",
           method: "POST",
-          body: { type: "GITHUB", username: user.email },
+          body: {
+            type: account?.provider.toLocaleUpperCase(),
+            username: user.email,
+          },
         });
         if (res.data) {
-          token = res.data;
+          token.access_token = res.data?.access_token;
+          token.refresh_token = res.data?.refresh_token;
+          token.user = res.data?.user;
         }
+      }
+      if (trigger === "signIn" && account?.provider === "credentials") {
+        //locall data lưu data vào token
+        //@ts-ignore
+        token.access_token = user.access_token;
+        //@ts-ignore
+        token.refresh_token = user.refresh_token;
+        //@ts-ignore
+        token.user = user.user;
       }
       return token;
     },
