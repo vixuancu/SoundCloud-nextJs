9729446071471b25ad0deca9d8e70fafe2e20194
vixuancu <vixuancu2004@gmail.com>
2025-02-07 11:09:15 +0700

    Config Github Provider

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,16 @@
+import NextAuth, { AuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+export const authOptions: AuthOptions = {
+  secret: process.env.NO_SECRET,
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!, // thêm dấu ! để không báo lỗi undefined
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+    // ...add more providers here
+  ],
+};
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
