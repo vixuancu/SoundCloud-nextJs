28421ab085379233cd4985154dc553d2cb88389a
vixuancu <vixuancu2004@gmail.com>
2025-02-10 21:35:56 +0700

    tích hợp MUI tab và dropzone

---
+++ src/app/(user)/track/upload/page.tsx
@@ -0,0 +1,13 @@
+import UploadTabs from "@/components/track/upload.tabs";
+import { Container } from "@mui/material";
+
+const UploadPage = () => {
+  return (
+    <>
+      <Container>
+        <UploadTabs />
+      </Container>
+    </>
+  );
+};
+export default UploadPage;
