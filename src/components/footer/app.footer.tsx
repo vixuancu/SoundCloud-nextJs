7d9340334fa9b4807842a3024f49ab3228ec9428
vixuancu <vixuancu2004@gmail.com>
2025-02-13 13:58:16 +0700

    custom track footer

--- src/components/footer/app.footer.tsx
+++ src/components/footer/app.footer.tsx
@@ -10,32 +10,47 @@ const AppFooter = () => {
   if (!hasMounted) return <></>;
   console.log("check Backend", process.env.NEXT_PUBLIC_BACKEND_URL);
   return (
-    <AppBar
-      position="fixed"
-      color="primary"
-      sx={{ top: "auto", bottom: 0, background: "#f2f2f2" }}
-    >
-      <Container sx={{ display: "flex", gap: "10px" }}>
-        {" "}
-        <AudioPlayer
-          src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/tracks/hoidanit.mp3`}
-          volume={0.5}
-          style={{ boxShadow: "unset", background: "#f2f2f2" }}
-        />
-        <div
-          style={{
+    <div style={{ marginTop: 50 }}>
+      <AppBar
+        position="fixed"
+        color="primary"
+        sx={{
+          top: "auto",
+          bottom: 0,
+          background: "#f2f2f2",
+        }}
+      >
+        <Container
+          sx={{
             display: "flex",
-            flexDirection: "column",
-            alignItems: "start",
-            justifyContent: "center",
-            minWidth: 100,
+            gap: "10px",
+            ".rhap_main": {
+              gap: "30px",
+            },
           }}
         >
-          <div style={{ color: "#ccc" }}>DucPhuc</div>
-          <div style={{ color: "black" }}>Who am i ?</div>
-        </div>
-      </Container>
-    </AppBar>
+          {" "}
+          <AudioPlayer
+            layout="horizontal-reverse"
+            src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/tracks/hoidanit.mp3`}
+            volume={0.5}
+            style={{ boxShadow: "unset", background: "#f2f2f2" }}
+          />
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "start",
+              justifyContent: "center",
+              minWidth: 100,
+            }}
+          >
+            <div style={{ color: "#ccc" }}>DucPhuc</div>
+            <div style={{ color: "black" }}>Who am i ?</div>
+          </div>
+        </Container>
+      </AppBar>
+    </div>
   );
 };
 export default AppFooter;
